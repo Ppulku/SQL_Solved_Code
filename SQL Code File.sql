082e7de9bb675d401a0bca78082003928353f69f
Pranav Chandrakant Pulkundwar <98478759+Ppulku@users.noreply.github.com>
2023-09-18 14:22:30 -0400

    Update SQL Code File.sql

--- SQL Code File.sql
+++ SQL Code File.sql
@@ -1,44 +1,40 @@
---Pranav Pulkundwar
---002121679
+/* 
 
-/* Q1
-Year	Week Day	Monday	Tuesday	Wednesday	Thursday	Friday	Saturday	Sunday
-2006	Total Sales								8164751				6749180	
-2007	Total Sales	6639424	5590523				6379290		9967385				10042829
-2008	Total Sales					5814991				
+----------Q.1----------
 
-
+Year	Week Day	Monday	Tuesday	Wednesday Thursday Friday Saturday Sunday
+2006	Total Sales						  8164751  6749180	
+2007	Total Sales	6639424	5590523		  6379290	  9967385  10042829
+2008	Total Sales				  5814991
 
 */
 
-select datepart(yy, OrderDate) Year,
-       datepart(dw, OrderDate) as WeekDay,
-       cast(sum(TotalDue) as int) as TotalSales
-from Sales.SalesOrderHeader
-where year(OrderDate) between 2006 and 2008
-group by datepart(dw, OrderDate),  datepart(yy, OrderDate)
-having sum(TotalDue) > 5500000
-
 USE AdventureWorks2008R2;
 
+SELECT datepart(yy, OrderDate) YEAR,
+       datepart(dw, OrderDate) AS WeekDay,
+       CAST(sum(TotalDue) AS int) AS TotalSales
+FROM Sales.SalesOrderHeader
+WHERE YEAR(OrderDate) BETWEEN 2006 AND 2008
+GROUP BY datepart(dw, OrderDate),  datepart(yy, OrderDate)
+HAVING sum(TotalDue) > 5500000
 
-	SELECT Year,'Total Sales',	ISNULL([1], ' ') AS 'Monday' ,
-							ISNULL([2], ' ') AS 'Tuesday',
-							ISNULL([3], ' ') AS 'Wednesday',
-							ISNULL([4], ' ') AS 'Thursday',
-							ISNULL([5], ' ') AS 'Friday',
-							ISNULL([6], ' ') AS 'Saturday',
-							ISNULL([7], ' ') AS 'Sunday'
+SELECT YEAR,'Total Sales', ISNULL([1], ' ') AS 'Monday' ,
+			   ISNULL([2], ' ') AS 'Tuesday',
+			   ISNULL([3], ' ') AS 'Wednesday',
+			   ISNULL([4], ' ') AS 'Thursday',
+			   ISNULL([5], ' ') AS 'Friday',
+			   ISNULL([6], ' ') AS 'Saturday',
+			   ISNULL([7], ' ') AS 'Sunday'
 FROM
-(
-	select datepart(yy, OrderDate) Year,
-       datepart(dw, OrderDate) as WeekDay,
-       cast(sum(TotalDue) as int) as TotalSales
-from Sales.SalesOrderHeader
-where year(OrderDate) between 2006 and 2008
-group by datepart(dw, OrderDate),  datepart(yy, OrderDate)
-having sum(TotalDue) > 5500000
-) AS SourceTable
+	(SELECT datepart(yy, OrderDate) YEAR,
+	        datepart(dw, OrderDate) AS WeekDay,
+	        CAST(sum(TotalDue) AS int) AS TotalSales
+	FROM Sales.SalesOrderHeader
+	WHERE YEAR(OrderDate) BETWEEN 2006 AND 2008
+	GROUP BY datepart(dw, OrderDate),  datepart(yy, OrderDate)
+	HAVING sum(TotalDue) > 5500000
+	) AS SourceTable
 PIVOT
 (
 	SUM(TotalSales)
@@ -46,15 +42,14 @@ PIVOT
 	IN ([1], [2], [3], [4], [5], [6], [7])
 
 ) AS PivotTable
-	
-
-
---------------------------------------------------------------------------------------------------------------------------------------------------
-
+------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------
 
 
 	
-/* Q2
+/*
+
+----------Q.2----------
 
 Using AdventureWorks2008R2, write a query to retrieve 
 the order years and their order info.
@@ -76,48 +71,50 @@ the format specified below.
 */
 
 /*
-Year	TotalSales	Top 3 Order Values					Percentage
+Year	TotalSales	Top 3 Order Values			Percentage
 2006	34463848	170512.67, 166537.08, 165028.75		1.45
 2007	47171490	187487.83, 182018.63, 145454.37		1.09
 */
-
-
-*/	
 
 USE AdventureWorks2008R2;
 
 WITH TEMP AS
 (
-	SELECT YEAR(soh.OrderDate) YEAR, ProductID,SOH.TotalDue AS 'TTQ',
-    DENSE_RANK() OVER (Partition By YEAR(soh.OrderDate) ORDER BY SOH.TotalDue DESC) AS 'Rank'
-    FROM Sales.SalesOrderDetail SOD
-    join Sales.SalesOrderHeader SOH
-    ON SOD.SalesOrderID=SOH.SalesOrderID
-    group by YEAR(soh.OrderDate), SOD.ProductID,SOH.TotalDue
+SELECT 
+	YEAR(soh.OrderDate) YEAR,
+	ProductID,
+	SOH.TotalDue AS 'TTQ',
+	DENSE_RANK() OVER (Partition BY YEAR(soh.OrderDate) ORDER BY SOH.TotalDue DESC) AS 'Rank'
+FROM Sales.SalesOrderDetail SOD
+JOIN Sales.SalesOrderHeader SOH
+	ON SOD.SalesOrderID=SOH.SalesOrderID
+GROUP BY YEAR(soh.OrderDate), SOD.ProductID, SOH.TotalDue
 )
-SELECT TEMP.YEAR,TTQ1 as [TotSales], (( CAST(SUM(TTQ) AS FLOAT)  / CAST(TTQ1 AS FLOAT) ) * 100 ) AS 'Percentage',
-STUFF((SELECT TOP 3  ', '+RTRIM(CAST(SOH.TotalDue AS char))  
-       FROM Sales.SalesOrderDetail SOD 
-       JOIN Sales.SalesOrderHeader SOH
-       ON SOD.SalesOrderID=SOH.SalesOrderID
-       WHERE YEAR(soh.OrderDate) = TEMP.YEAR
-	   GROUP BY SOH.TotalDue
-       ORDER BY  SOH.TotalDue  DESC
-       FOR XML PATH('')) , 1, 1, '') AS 'Top3Order Values'
+SELECT 
+	TEMP.YEAR,
+	TTQ1 AS [TotSales],
+	(( CAST(SUM(TTQ) AS FLOAT)  / CAST(TTQ1 AS FLOAT) ) * 100 ) AS 'Percentage',
+	STUFF(
+		(
+		SELECT TOP 3  ', '+RTRIM(CAST(SOH.TotalDue AS char))  
+		FROM Sales.SalesOrderDetail SOD 
+		JOIN Sales.SalesOrderHeader SOH
+			ON SOD.SalesOrderID=SOH.SalesOrderID
+		WHERE YEAR(soh.OrderDate) = TEMP.YEAR
+		GROUP BY SOH.TotalDue
+		ORDER BY  SOH.TotalDue  DESC
+		FOR XML PATH('')
+		) , 1, 1, ''
+	     ) AS 'Top3Order Values'
 FROM TEMP 
 JOIN 
-(
-	SELECT YEAR(soh.OrderDate) YEAR, SUM(SOH.TotalDue) AS 'TTQ1'
- 	FROM Sales.SalesOrderHeader SOH
-    --JOIN Sales.SalesOrderHeader SOH
-    --ON SOD.SalesOrderID=SOH.SalesOrderID
-    GROUP BY YEAR(soh.OrderDate)
-      ) B
+(SELECT YEAR(soh.OrderDate) YEAR, SUM(SOH.TotalDue) AS 'TTQ1'
+ FROM Sales.SalesOrderHeader SOH
+GROUP BY YEAR(soh.OrderDate)) B
 ON TEMP.YEAR =B.YEAR 
 WHERE TEMP.Rank <=3
 GROUP BY TEMP.YEAR,B.TTQ1 
 ORDER BY TEMP.YEAR;
-
 
 USE AdventureWorks2008R2;
 
